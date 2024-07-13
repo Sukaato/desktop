bf639fc9680266154b930de887d0438e482f98a1
Sukaato <sukaato.dev@proton.me>
2024-07-13 22:17:46 +0200

    Update install.ps1
    move C++ lib in first

--- install.ps1
+++ install.ps1
@@ -3,6 +3,22 @@ $C_GREEN = [char]27 + "[1;32m"
 
 Write-Output "${C_GREEN}desktop-install v0.0.0-alpha.1${C_RESET}"
 
+# Intall in first place all C++ lib since some software need it
+# Microsoft C++
+winget add -e --id Microsoft.VCRedist.2005.x86 
+winget add -e --id Microsoft.VCRedist.2005.x64
+winget add -e --id Microsoft.VCRedist.2008.x86 
+winget add -e --id Microsoft.VCRedist.2008.x64
+winget add -e --id Microsoft.VCRedist.2010.x86 
+winget add -e --id Microsoft.VCRedist.2010.x64
+winget add -e --id Microsoft.VCRedist.2012.x86 
+winget add -e --id Microsoft.VCRedist.2012.x64
+winget add -e --id Microsoft.VCRedist.2013.x86 
+winget add -e --id Microsoft.VCRedist.2013.x64
+winget add -e --id Microsoft.VCRedist.2015+.x86 
+winget add -e --id Microsoft.VCRedist.2015+.x64
+
+
 # Dev
 winget add -e --id Microsoft.VisualStudioCode -l "D:\programs\visualStudioCode" --custom '/mergetasks="!runcode,addcontextmenufiles,addcontextmenufolders"'
 winget add -e --id Git.Git -l "D:\programs\git" --custom '/Components=gitlfs,scalar,assoc,assoc_sh,autoupdate' -i
@@ -23,7 +39,6 @@ winget add -e --id PrismLauncher.PrismLauncher -l "D:\games\primsLauncher"
 # Communication
 winget add -e --id Discord.Discord
 
-
 # Tools
 winget add -e --id 7zip.7zip -l "D:\programs\7z"
 winget add -e --id Brave.Brave -l "D:\programs\brave" -i
@@ -33,20 +48,6 @@ winget add -e --id VideoLAN.VLC -l "D:\programs\vlc"
 winget add -e --id DelugeTeam.Deluge -l "D:\programs\deluge"
 winget add -e --id Nilesoft.Shell -l "D:\programs\nilesoftShell"
 winget add -e --id Parsec.Parsec -l "D:\programs\parsec" -i
-
-# Microsoft C++
-winget add -e --id Microsoft.VCRedist.2005.x86 
-winget add -e --id Microsoft.VCRedist.2005.x64
-winget add -e --id Microsoft.VCRedist.2008.x86 
-winget add -e --id Microsoft.VCRedist.2008.x64
-winget add -e --id Microsoft.VCRedist.2010.x86 
-winget add -e --id Microsoft.VCRedist.2010.x64
-winget add -e --id Microsoft.VCRedist.2012.x86 
-winget add -e --id Microsoft.VCRedist.2012.x64
-winget add -e --id Microsoft.VCRedist.2013.x86 
-winget add -e --id Microsoft.VCRedist.2013.x64
-winget add -e --id Microsoft.VCRedist.2015+.x86 
-winget add -e --id Microsoft.VCRedist.2015+.x64
 
 Write-Output "${C_GREEN}Installation terminé${C_RESET}"
 
