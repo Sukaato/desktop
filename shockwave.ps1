6ec9acff47160e99bb2f72556ce8e8d595247f30
Sukaato <sukaato.dev@proton.me>
2024-07-20 18:54:09 +0200

    Create shockwave.ps1
    ad install script for shockwave

---
+++ shockwave.ps1
@@ -0,0 +1,28 @@
+winget add -e --id Opera.OperaGX
+winget add -e --id Ubisoft.Connect
+winget add -e --id Valve.Steam
+winget add -e --id ElectronicArts.EADesktop
+winget add -e --id EpicGames.EpicGamesLauncher
+winget add -e --id Discord.Discord
+winget add -e --id Spotify.Spotify
+winget add -e --id OBSProject.OBSStudio
+winget add -e --id Elgato.StreamDeck
+winget add -e --id SteelSeries.GG
+winget add -e --id 7zip.7zip
+winget add -e --id VideoLAN.VLC
+winget add -e --id Voicemod.Voicemod
+
+winget add -e --id Microsoft.VCRedist.2005.x86 
+winget add -e --id Microsoft.VCRedist.2005.x64
+winget add -e --id Microsoft.VCRedist.2008.x86 
+winget add -e --id Microsoft.VCRedist.2008.x64
+winget add -e --id Microsoft.VCRedist.2010.x86 
+winget add -e --id Microsoft.VCRedist.2010.x64
+winget add -e --id Microsoft.VCRedist.2012.x86 
+winget add -e --id Microsoft.VCRedist.2012.x64
+winget add -e --id Microsoft.VCRedist.2013.x86 
+winget add -e --id Microsoft.VCRedist.2013.x64
+winget add -e --id Microsoft.VCRedist.2015+.x86 
+winget add -e --id Microsoft.VCRedist.2015+.x64
+
+winget update -r -u
